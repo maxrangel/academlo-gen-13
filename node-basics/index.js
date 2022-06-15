0b9b1c7c8d202d4514eeecf60ac0e04e604640d2
maxrangel <lik4ever@hotmail.com>
2022-06-14 21:54:48 -0500

    created first endpoints

--- node-basics/index.js
+++ node-basics/index.js
@@ -1,7 +1,38 @@
 const http = require('http'); // CommonJS
 
-const server = http.createServer(() => {
-	console.log('Hello from server');
+// Dummy data
+const users = [
+	{ id: 1, name: 'Max' },
+	{ id: 2, name: 'Luis' },
+	{ id: 3, name: 'John' },
+];
+
+const posts = [
+	{ id: 1, title: 'Post #1' },
+	{ id: 2, title: 'Post #2' },
+	{ id: 3, title: 'Post #3' },
+];
+
+const server = http.createServer((request, response) => {
+	const { method, url } = request;
+
+	response.setHeader('Content-Type', 'application/json');
+
+	// Endpoint GET /users
+	if (method === 'GET' && url === '/users') {
+		// Return list of users
+		response.write(JSON.stringify(users));
+	} else if (method === 'GET' && url === '/posts') {
+		// Return list of posts
+		response.write(JSON.stringify(posts));
+	} else {
+		// No endpoint matched
+		response.write(
+			JSON.stringify({ message: `${method} ${url} not found in this server` })
+		);
+	}
+
+	response.end();
 });
 
 // localhost:4000
