58f84cc3b2cd3ce5a78c0242789d48239a446a17
maxrangel <lik4ever@hotmail.com>
2022-06-16 22:07:27 -0500

    connected to db

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,17 +1,68 @@
 const express = require('express');
+const { Sequelize, DataTypes } = require('sequelize');
 
-// Dummy data
-const users = [
-	{ id: 1, name: 'Max' },
-	{ id: 2, name: 'Luis' },
-	{ id: 3, name: 'John' },
-];
+// Connect to database
+const db = new Sequelize({
+	dialect: 'postgres',
+	host: 'localhost',
+	username: 'postgres',
+	password: 'pass1234',
+	port: 5432,
+	database: 'facebook',
+});
 
-const posts = [
-	{ id: 1, title: 'Post #1' },
-	{ id: 2, title: 'Post #2' },
-	{ id: 3, title: 'Post #3' },
-];
+db.authenticate()
+	.then(() => console.log('Db authenticated'))
+	.catch(err => console.log(err));
+
+db.sync()
+	.then(() => console.log('Db synced'))
+	.catch(err => console.log(err));
+
+// Create our first model (table)
+const User = db.define('user', {
+	id: {
+		primaryKey: true,
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	age: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true,
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	status: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		defaultValue: 'active',
+	},
+});
+
+// // Dummy data
+// const users = [
+// 	{ id: 1, name: 'Max' },
+// 	{ id: 2, name: 'Luis' },
+// 	{ id: 3, name: 'John' },
+// ];
+
+// const posts = [
+// 	{ id: 1, title: 'Post #1' },
+// 	{ id: 2, title: 'Post #2' },
+// 	{ id: 3, title: 'Post #3' },
+// ];
 
 // Init express app
 const app = express();
@@ -19,12 +70,17 @@ const app = express();
 app.use(express.json());
 
 // Define endpoints
-app.get('/users', (req, res) => {
-	// Process the request (return the list the users)
-	res.status(200).json({
-		status: 'success',
-		users,
-	});
+app.get('/users', async (req, res) => {
+	try {
+		const users = await User.findAll();
+
+		res.status(200).json({
+			status: 'success',
+			users,
+		});
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 app.post('/users', (req, res) => {
