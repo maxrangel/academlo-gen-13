d0932e9afd6114529a65e5fd1c75f066beedf3d3
maxrngl <lik4ever@hotmail.com>
2022-07-07 20:43:09 -0500

    working with pug

--- heroku-example/app.js
+++ heroku-example/app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 
 // Set template engine
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
--- heroku-example/controllers/views.controller.js
+++ heroku-example/controllers/views.controller.js
@@ -9,9 +9,15 @@ const { catchAsync } = require('../utils/catchAsync.util');
 const renderIndex = catchAsync(async (req, res, next) => {
 	const posts = await Post.findAll();
 
-	const indexPath = path.join(__dirname, '..', 'public', 'index.html');
+	res.status(200).render('index', {
+		title: 'Rendered with Pug',
+		posts,
+	});
 
-	res.status(200).sendFile(indexPath);
+	// Serve static html
+	// const indexPath = path.join(__dirname, '..', 'public', 'index.html');
+
+	// res.status(200).sendFile(indexPath);
 });
 
 module.exports = { renderIndex };
