dc0835fb1bc07fee6ae321503ba4ec329efe6fef
maxrngl <lik4ever@hotmail.com>
2022-07-25 21:41:20 -0500

    finished add product to cart feature

--- ecommerce-project/controllers/orders.controller.js
+++ ecommerce-project/controllers/orders.controller.js
@@ -1,11 +1,65 @@
+// Models
+const { Cart } = require('../models/cart.model');
+const { Product } = require('../models/product.model');
+const { ProductInCart } = require('../models/productInCart.model');
+
 // Utils
 const { catchAsync } = require('../utils/catchAsync.util');
+const { AppError } = require('../utils/appError.util');
 
 const getUserCart = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: 'success' });
 });
 
 const addProductToCart = catchAsync(async (req, res, next) => {
+  const { sessionUser } = req;
+  const { productId, quantity } = req.body;
+
+  // Validate input qty
+  const product = await Product.findOne({
+    where: { id: productId, status: 'active' },
+  });
+
+  if (!product) {
+    return next(new AppError('Invalid product', 404));
+  } else if (quantity > product.quantity) {
+    return next(
+      new AppError(
+        `This product only has ${product.quantity} items available`,
+        400
+      )
+    );
+  }
+
+  // Check if cart exists
+  const cart = await Cart.findOne({
+    where: { status: 'active', userId: sessionUser.id },
+  });
+
+  if (!cart) {
+    // Create new cart for user
+    const newCart = await Cart.create({ userId: sessionUser.id });
+
+    // Add product to newly created cart
+    await ProductInCart.create({
+      cartId: newCart.id,
+      productId,
+      quantity,
+    });
+  } else {
+    // Cart already exists
+    // Check if product already exists in cart
+    const productExists = await ProductInCart.findOne({
+      where: { cartId: cart.id, productId },
+    });
+
+    if (productExists) {
+      return next(new AppError('Product is already in the cart', 400));
+    }
+
+    await ProductInCart.create({ cartId: cart.id, productId, quantity });
+  }
+
   res.status(200).json({ status: 'success' });
 });
 
