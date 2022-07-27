3559409f6f51b5ca943ebe71cf1fae6ad8fd994d
maxrngl <lik4ever@hotmail.com>
2022-07-26 22:17:27 -0500

    working on purchase function

--- ecommerce-project/controllers/orders.controller.js
+++ ecommerce-project/controllers/orders.controller.js
@@ -8,7 +8,25 @@ const { catchAsync } = require('../utils/catchAsync.util');
 const { AppError } = require('../utils/appError.util');
 
 const getUserCart = catchAsync(async (req, res, next) => {
-  res.status(200).json({ status: 'success' });
+  const { sessionUser } = req;
+
+  const cart = await Cart.findOne({
+    where: { userId: sessionUser.id, status: 'active' },
+    include: [
+      {
+        model: ProductInCart,
+        required: false,
+        where: { status: 'active' },
+        include: { model: Product },
+      },
+    ],
+  });
+
+  if (!cart) {
+    return next(new AppError('Cart not found', 404));
+  }
+
+  res.status(200).json({ status: 'success', cart });
 });
 
 const addProductToCart = catchAsync(async (req, res, next) => {
@@ -64,14 +82,117 @@ const addProductToCart = catchAsync(async (req, res, next) => {
 });
 
 const updateProductInCart = catchAsync(async (req, res, next) => {
+  const { sessionUser } = req;
+  const { productId, newQty } = req.body;
+
+  // Validate input qty
+  const product = await Product.findOne({
+    where: { id: productId, status: 'active' },
+  });
+
+  if (!product) {
+    return next(new AppError('Invalid product', 404));
+  } else if (newQty > product.quantity) {
+    return next(
+      new AppError(
+        `This product only has ${product.quantity} items available`,
+        400
+      )
+    );
+  }
+
+  const cart = await Cart.findOne({
+    where: { userId: sessionUser.id, status: 'active' },
+  });
+
+  if (!cart) {
+    return next(new AppError('Cart not found', 404));
+  }
+
+  const productInCart = await ProductInCart.findOne({
+    cartId: cart.id,
+    productId,
+    status: 'active',
+  });
+
+  if (!productInCart) {
+    return next(new AppError('Product not found in cart', 404));
+  }
+
+  if (newQty <= 0) {
+    await productInCart.update({ quantity: 0, status: 'removed' });
+  } else if (newQty > 0) {
+    await productInCart.update({ quantity: newQty });
+  }
+
   res.status(200).json({ status: 'success' });
 });
 
 const purchaseCart = catchAsync(async (req, res, next) => {
+  const { sessionUser } = req;
+
+  const cart = await Cart.findOne({
+    where: { userId: sessionUser.id, status: 'active' },
+    include: [
+      {
+        model: ProductInCart,
+        required: false,
+        where: { status: 'active' },
+        include: { model: Product },
+      },
+    ],
+  });
+
+  if (!cart) {
+    return next(new AppError('Cart not found', 404));
+  }
+
+  let totalPrice = 0;
+
+  const productsPurchasedPromises = cart.productInCarts.map(
+    async productInCart => {
+      const newQty = productInCart.product.quantity - productInCart.quantity;
+
+      const productPrice =
+        productInCart.quantity * +productInCart.product.price;
+
+      totalPrice += productPrice;
+
+      await productInCart.product.update({ quantity: newQty });
+
+      return await productInCart.update({ status: 'purchased' });
+    }
+  );
+
+  await Promise.all(productsPurchasedPromises);
+
   res.status(200).json({ status: 'success' });
 });
 
 const removeProductFromCart = catchAsync(async (req, res, next) => {
+  const { sessionUser } = req;
+  const { productId } = req.params;
+
+  const cart = await Cart.findOne({
+    where: { userId: sessionUser.id, status: 'active' },
+  });
+
+  if (!cart) {
+    return next(new AppError('Cart not found', 404));
+  }
+
+  const productInCart = await ProductInCart.findOne({
+    cartId: cart.id,
+    productId,
+    status: 'active',
+  });
+
+  if (!productInCart) {
+    return next(new AppError('Product not found in cart', 404));
+  }
+
+  await productInCart.update({ status: 'removed', quantity: 0 });
+
   res.status(200).json({ status: 'success' });
 });
 
